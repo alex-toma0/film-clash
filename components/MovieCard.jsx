2393f91da8a15a1fb850f96cd3a486a4286286e7
Alex <alextoma182@gmail.com>
2022-08-05 21:38:29 +0300

    Fixed issue where title wasn't lining up properly

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -11,7 +11,7 @@ export default function MovieCard(props){
         <div className="flex flex-col w-1/3 h-full items-center justify-center align-middle break-words">
             <img src={imgURL} className="w-30 md:w-4/5 lg:w-1/2"/>
             <span onClick={handleGuess} className="md:text-lg mt-3 p-0 text-sm text-center text-red-400 
-            font-semibold hover:text-blue-600 cursor-pointer h-1/2" >{props.movie.title}</span>
+            font-semibold hover:text-blue-600 cursor-pointer h-10" >{props.movie.title}</span>
         </div>
     )
 }
